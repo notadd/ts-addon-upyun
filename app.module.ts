2a096cf14d53fac3b756b50cc6463f2810b06d10
czy12345 <1253665625@qq.com>
2018-01-15 18:22:01 +0800

    添加graphql基础

--- app.module.ts
+++ app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module,MiddlewaresConsumer,NestModule,RequestMethod, } from '@nestjs/common';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { GraphQLModule , GraphQLFactory} from '@nestjs/graphql';
 import { UpyunModule } from './src/upyun/UpyunModule'
-import { TestController } from './test/TestController'
 @Module({
-  modules: [UpyunModule],
-  controllers: [TestController],
+  modules: [UpyunModule,GraphQLModule],
+  controllers: [],
   components: []
 })
 
 
-export class ApplicationModule {}
+export class ApplicationModule implements NestModule{
+
+  constructor(private readonly graphQLFactory: GraphQLFactory){}
+
+  configure(consumer: MiddlewaresConsumer) {
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths('./src/upyun/graphql/type/*.graphql');
+    const schema = this.graphQLFactory.createSchema({ typeDefs });
+    consumer
+      .apply(graphiqlExpress({ endpointURL: '/graphql' }))
+      .forRoutes({ path: '/graphiql', method: RequestMethod.GET })
+      .apply(graphqlExpress(req => ({ schema, rootValue: req })))
+      .forRoutes({ path: '/graphql', method: RequestMethod.ALL });
+  }
+}
