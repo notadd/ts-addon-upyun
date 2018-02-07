13029fade9681d8784288b524c5e867919b374e1
czy12345 <1253665625@qq.com>
2018-02-07 14:46:10 +0800

    save

--- src/graphql/resolver/FileResolver.ts
+++ src/graphql/resolver/FileResolver.ts
@@ -56,11 +56,11 @@ export class FileResolver {
     try {
       let { bucketName, name, type } = body
       if (!bucketName || !name) {
-        throw new HttpException('缺少参数',400)
+        throw new HttpException('缺少参数', 400)
       }
       let bucket: Bucket = await this.bucketRepository.findOne({ name: bucketName })
       if (!bucket) {
-        throw new HttpException('指定空间' + bucketName + '不存在',401)
+        throw new HttpException('指定空间' + bucketName + '不存在', 401)
       }
       let kind
       let status = 'post'
@@ -71,7 +71,7 @@ export class FileResolver {
         //其他类型暂不支持
       }
       if (!file) {
-        throw new HttpException('指定文件' + name + '不存在',402)
+        throw new HttpException('指定文件' + name + '不存在', 402)
       }
       data.url += '/' + bucket.name + '/' + bucket.directory + '/' + file.name + '.' + file.type
       data.headers.date = new Date(+new Date() + bucket.request_expire * 1000).toUTCString()
@@ -91,10 +91,10 @@ export class FileResolver {
 
 
   @Mutation('uploadProcess')
-  async uploadProcess(req: any, body: UploadProcessBody): Promise<UploadProcessData> {
+  async uploadProcess(req: IncomingMessage, body: UploadProcessBody): Promise<UploadProcessData> {
     let data: UploadProcessData = {
       code: 200,
-      message: '',
+      message: '上传预处理成功',
       url: 'http://v0.api.upyun.com',
       method: 'post',
       form: {
@@ -102,54 +102,55 @@ export class FileResolver {
         authorization: ''
       }
     }
-    let { bucketName, md5, contentName } = body
-    if (!bucketName || !md5 || !contentName) {
-      data.code = 400
-      data.message = '缺少参数'
-      return data
+    try {
+      let { bucketName, md5, contentName } = body
+      if (!bucketName || !md5 || !contentName) {
+        throw new HttpException('缺少参数', 400)
+      }
+      if (md5.length !== 32) {
+        throw new HttpException('md5参数不正确', 400)
+      }
+      //查询空间配置，关联查询图片、音频、视频配置，处理文件需要这些信息
+      let bucket: Bucket = await this.bucketRepository.createQueryBuilder("bucket")
+        .leftJoinAndSelect("bucket.image_config", "image_config")
+        .leftJoinAndSelect("bucket.audio_config", "audio_config")
+        .leftJoinAndSelect("bucket.video_config", "video_config")
+        .where("bucket.name = :name", { name: bucketName })
+        .getOne()
+      if (!bucket) {
+        throw new HttpException('指定空间' + bucketName + '不存在', 401)
+      }
+      //预保存图片,获取保存的图片，图片名为预处理图片名，会设置到policy的apps中去
+      let image = await this.fileService.preSaveFile(bucket, body)
+      //上传policy字段
+      let policy: Policy = {
+        //空间名
+        'bucket': '',
+        //文件保存路径，包括目录、文件名、扩展名
+        'save-key': '',
+        //请求过期事件
+        'expiration': null,
+        'date': '',
+        'content-md5': md5,
+        //异步回调通知路径，图片异步预处理回调也是这个接口
+        'notify-url': 'http://upyuns.frp2.chuantou.org/upyun/file/notify',
+        //图片生存期限默认为180天
+        'x-upyun-meta-ttl': 180,
+        //扩展参数，包含了空间名
+        'ext-param': ''
+      }
+      //获取后台配置，创建上传参数，返回文件种类、以及文件所属目录
+      await this.fileService.makePolicy(data, policy, bucket, body, image)
+    } catch (err) {
+      if (err instanceof HttpException) {
+        data.code = err.getStatus()
+        data.message = err.getResponse() + ''
+      } else {
+        console.log(err)
+        data.code = 500
+        data.message = '出现了意外错误' + err.toString()
+      }
     }
-    if (md5.length !== 32) {
-      data.code = 400
-      data.message = 'md5参数不正确'
-      return data
-    }
-    //查询空间配置，关联查询图片、音频、视频配置，处理文件需要这些信息
-    let bucket: Bucket = await this.bucketRepository.createQueryBuilder("bucket")
-      .leftJoinAndSelect("bucket.image_config", "image_config")
-      .leftJoinAndSelect("bucket.audio_config", "audio_config")
-      .leftJoinAndSelect("bucket.video_config", "video_config")
-      .where("bucket.name = :name", { name: bucketName })
-      .getOne()
-    if (!bucket) {
-      data.code = 401
-      data.message = '指定空间' + bucketName + '不存在'
-      return
-    }
-    //预保存图片,获取保存的图片，图片名为预处理图片名，会设置到policy的apps中去
-    let image = await this.fileService.preSaveFile(data, bucket, body)
-    //图片保存失败
-    if (data.code == 402) {
-      return data
-    }
-    //上传policy字段
-    let policy: Policy = {
-      //空间名
-      'bucket': '',
-      //文件保存路径，包括目录、文件名、扩展名
-      'save-key': '',
-      //请求过期事件
-      'expiration': null,
-      'date': '',
-      'content-md5': md5,
-      //异步回调通知路径，图片异步预处理回调也是这个接口
-      'notify-url': 'http://upyuns.frp2.chuantou.org/upyun/file/notify',
-      //图片生存期限默认为180天
-      'x-upyun-meta-ttl': 180,
-      //扩展参数，包含了空间名
-      'ext-param': ''
-    }
-    //获取后台配置，创建上传参数，返回文件种类、以及文件所属目录
-    await this.fileService.makePolicy(data, policy, bucket, body, image)
     return data
   }
 
@@ -163,46 +164,51 @@ export class FileResolver {
             data.url：访问文件的全部url，包括域名、目录、文件名、扩展名、token、文件密钥、处理字符串
  */
   @Query('one')
-  async  getFile(req: any, body: OneBody): Promise<OneData> {
+  async  getFile(req: IncomingMessage, body: OneBody): Promise<OneData> {
     let data: OneData = {
       code: 200,
-      message: "",
+      message: "获取指定文件访问url成功",
       url: ''
     }
-    //验证参数存在
-    let { bucketName, name, type } = body
-    if (!bucketName || !name || !type) {
-      data.code = 400
-      data.message = '缺少参数'
-      return data
-    }
-    let bucket: Bucket = await this.bucketRepository.createQueryBuilder("bucket")
-      .leftJoinAndSelect("bucket.image_config", "image_config")
-      .leftJoinAndSelect("bucket.audio_config", "audio_config")
-      .leftJoinAndSelect("bucket.video_config", "video_config")
-      .where("bucket.name = :name", { name: bucketName })
-      .getOne()
-    if (!bucket) {
-      data.code = 401
-      data.message = '空间不存在'
-      return data
-    }
-    //根据种类获取不同url
-    let kind = this.kindUtil.getKind(type)
-    let file: File | Audio | Video | Image | Document
-    //处理图片类型
-    if (kind === 'image') {
-      file = await this.imageRepository.findOne({ name, bucketId: bucket.id })
-      if (!file) {
-        data.code = 402
-        data.message = '指定图片不存在'
-        return data
+    try {
+      //验证参数存在
+      let { bucketName, name, type } = body
+      if (!bucketName || !name || !type) {
+        throw new HttpException('缺少参数',400)
       }
-    } else {
-      //暂不支持
+      let bucket: Bucket = await this.bucketRepository.createQueryBuilder("bucket")
+        .leftJoinAndSelect("bucket.image_config", "image_config")
+        .leftJoinAndSelect("bucket.audio_config", "audio_config")
+        .leftJoinAndSelect("bucket.video_config", "video_config")
+        .where("bucket.name = :name", { name: bucketName })
+        .getOne()
+      if (!bucket) {
+        throw new HttpException('指定空间'+bucketName+'不存在',401)
+      }
+      //根据种类获取不同url
+      let kind = this.kindUtil.getKind(type)
+      let file: File | Audio | Video | Image | Document
+      //处理图片类型
+      if (kind === 'image') {
+        file = await this.imageRepository.findOne({ name, bucketId: bucket.id })
+        if (!file) {
+          throw new HttpException('指定图片不存在',402)
+        }
+      } else {
+        //暂不支持
+      }
+      //所有文件调用统一的拼接Url方法
+      await this.fileService.makeUrl(data, bucket, file, body, kind)
+    } catch (err) {
+      if (err instanceof HttpException) {
+        data.code = err.getStatus()
+        data.message = err.getResponse() + ''
+      } else {
+        console.log(err)
+        data.code = 500
+        data.message = '出现了意外错误' + err.toString()
+      }
     }
-    //所有文件调用统一的拼接Url方法
-    await this.fileService.makeUrl(data, bucket, file, body, kind)
     return data
   }
 
