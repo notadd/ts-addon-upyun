442bc86ebffa925c72a51fa64fb8dfff0a932a44
czy12345 <1253665625@qq.com>
2018-04-18 15:54:10 +0800

    reokace ExceptionInterceptor

--- src/interceptor/exception.interceptor.ts
+++ src/interceptor/exception.interceptor.ts
@@ -1,21 +1,27 @@
-
-import { ExecutionContext, HttpException, Interceptor, NestInterceptor } from "@nestjs/common";
-import "rxjs/add/operator/catch";
+import { Interceptor, NestInterceptor, ExecutionContext, HttpException } from "@nestjs/common";
 import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/catch";
 
 @Interceptor()
 export class ExceptionInterceptor implements NestInterceptor {
+
     intercept(dataOrRequest, context: ExecutionContext, stream$: Observable<any>): Observable<any> {
+
         return stream$.catch((err, caught): any => {
             if (err instanceof HttpException) {
                 return Promise.resolve({
                     code: err.getStatus(),
                     message: err.getResponse()
                 });
+            } else if (err instanceof Error) {
+                return Promise.resolve({
+                    code: 500,
+                    message: "出现了意外错误:" + err.name + "\n" + err.message + "\n" + err.stack
+                });
             } else {
                 return Promise.resolve({
                     code: 500,
-                    message: "出现了意外错误" + err.toString()
+                    message: "出现了意外错误:" + err.toString()
                 });
             }
         });
